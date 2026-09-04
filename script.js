41a05fdf8d7fa4f9320632483ca9ab2c97563525
dev-l-k <devlkakkoth545@gmail.com>
2026-09-04 07:37:00 +0530

    add theme toggle

---
+++ script.js
@@ -0,0 +1,28 @@
+const themeToggle = document.getElementById("theme-toggle");
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "light"){
+    document.documentElement.dataset.theme ="light";
+    themeToggle.textContent="☀️"
+
+}
+themeToggle.addEventListener("click",() => {
+    const isLight = document.documentElement.dataset.theme === "light";
+    if (isLight){
+        document.documentElement.removeAttribute("data-theme");
+        themeToggle.textContent="🌙";
+        localStorage.setItem("theme","dark");
+    }else{
+        document.documentElement.dataset.theme="light";
+        themeToggle.textContent="☀️";
+        localStorage.setItem("theme","light");
+    }
+
+});
+const observer = new IntersectionObserver(("entries") => {
+    CustomElementRegistry.forEach((entry) => {
+        if (entry.isIntersecting){
+            entry.target.classList.add("visible");
+            observer.un
+        }
+    })
+})
